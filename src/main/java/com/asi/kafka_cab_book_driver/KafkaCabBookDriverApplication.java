b57d4c440372d52cb39d78d50b1e63c151bc22bc
asitha <cstasitha@gmail.com>
2024-07-01 02:23:06 +0530

    initial commit

---
+++ src/main/java/com/asi/kafka_cab_book_driver/KafkaCabBookDriverApplication.java
@@ -0,0 +1,13 @@
+package com.asi.kafka_cab_book_driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCabBookDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCabBookDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asi/kafka_cab_book_driver/KafkaCabBookDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi.kafka_cab_book_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCabBookDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
